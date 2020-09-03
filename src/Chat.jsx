6aac1ece994aeaafc4d9939c05b76756fbcbce9b
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-03 16:50:29 -0300

    Pegando as mensagens do servidor com gql e usequery

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache()
 });
 
+const GET_MESSAGES = gql`
+    query {
+    messages {
+        id
+        content
+        user
+    }
+    }
+`;
+
+const Messages = ({user}) => {
+    const {data} = useQuery(GET_MESSAGES);
+
+    if(!data) {
+        return null;
+    }
+
+    return JSON.stringify(data);
+}
+
 const Chat = () => {
-    return <div>Janela do chat!</div>;
+    return <div><Messages user='Molezinha' /></div>;
     
 };
 
