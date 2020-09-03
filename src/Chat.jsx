90f3c7b37decd4af9660378881fb619e8e3e59f5
Jefferson Brandão <jeffersonbraster@gmail.com>
2020-09-03 17:15:18 -0300

    Estilizando e preparando para envio de mensagens

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import {Container, Row, Col, FormInput, Buttom} from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -7,13 +8,13 @@ const client = new ApolloClient({
 });
 
 const GET_MESSAGES = gql`
-    query {
+query {
     messages {
         id
         content
         user
     }
-    }
+}
 `;
 
 const Messages = ({user}) => {
@@ -23,12 +24,91 @@ const Messages = ({user}) => {
         return null;
     }
 
-    return JSON.stringify(data);
+    return (
+        <>
+            {data.messages.map(({id, user: messageUser, content}) => (
+                <div style={{display: 'flex', 
+                justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+                paddingBottom: '1em',
+                }}>
+                    {user !== messageUser && (
+                        <div style={{
+                            height: 50, 
+                            width: 50, 
+                            marginRight: '0.5em', 
+                            border: '2px solid #e5e6ea', 
+                            borderRadius: 25, 
+                            textAlign: 'center', 
+                            fontSize: '18pt',
+                            paddingTop: 5}}>
+                            {messageUser.slice(0, 2).toUpperCase()}
+                        </div>
+                    )}
+                    <div style={{display: 'flex', 
+                                background: user === messageUser ? '#58bf56' : '#e5e6ea',
+                                color: user === messageUser ? 'white' : 'black',
+                                padding: '1em',
+                                borderRadius: '1em',
+                                maxWidth: '60%',
+                                }}>
+                        {content}
+                    </div>
+                </div>
+            ))}
+        </>
+    );
 }
 
 const Chat = () => {
-    return <div><Messages user='Molezinha' /></div>;
-    
+    const [state, setState] = React.useState({
+        user: 'Molezinha',
+        content: '',
+    });
+
+    const onSend = () => {
+        if(state.content.length > 0) {
+
+        }
+        stateSet({
+            ...state,
+            content: '',
+        })
+    }
+
+    return (
+        <Container>
+            <Messages user={state.user} />
+            <Row>
+                <Col xs={2} style={{padding: 0}}>
+                    <FormInput label="User" value={state.us} 
+                    onChange={(evt) => stateSet({
+                        ...state,
+                        user: evt.target.value,
+                    })} />
+                </Col>
+
+                <Col xs={8}>
+                    <FormInput label="Content" value={state.content} 
+                    onChange={(evt) => stateSet({
+                        ...state,
+                        user: evt.target.value,
+                    })}
+                        onkeyUp={(evt) => {
+                            if(evt.keyCode ===13) {
+                                onSend();
+                            }
+                        }}
+                    />
+                </Col>
+
+                <Col xs={2} style={{padding: 0}}>
+                    <Button onClick={() => onSend()}>
+                        Enviar
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
 };
 
 export default () => (
